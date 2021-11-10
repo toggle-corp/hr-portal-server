0e0ff323db8926b6e3b0bb38efaf3d5d94477b23
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-11-10 16:11:40 +0545

    set up media files

--- hr_portal/settings.py
+++ hr_portal/settings.py
@@ -97,14 +97,6 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -142,14 +134,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+# STATIC_URL = '/static/'
 
 if DEBUG:
     STATIC_URL = '/static/'
-    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    MEDIA_URL = 'media/'
+    STATIC_ROOT = BASE_DIR / '/static'
+
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
